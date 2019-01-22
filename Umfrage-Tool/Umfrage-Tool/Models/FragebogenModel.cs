a3f1a8a79338cbca5db42e7bb26b78aba02218fa
Raiko Schulz <SchulzRa@BAIT.BBW.DD>
2019-01-22 11:58:54 +0100

    Beantwortung-View implementiert und Startseite angepasst
    Models müssen noch an neue Datenbank-Models angepasst werden, nachdem diese erstellt wurden (von Max).

---
+++ Umfrage-Tool/Umfrage-Tool/Models/FragebogenModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Umfrage_Tool.Models
+{
+    public class FragebogenModel
+    {
+        public string Name { get; set; }
+        public ICollection<FragenModel> FragenListe { get; set; }
+    }
+}
