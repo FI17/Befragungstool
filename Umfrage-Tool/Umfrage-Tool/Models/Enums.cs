5e4685e20eb5795ef966bb5202158105734906f4
Toni <Toni5416@aol.com>
2019-05-02 15:16:37 +0200

    Status wechseln und authorisieren

---
+++ Umfrage-Tool/Umfrage-Tool/Models/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Umfrage_Tool
+{
+    public class Enums
+    {
+        public enum StatenTextAusgabe
+        {
+            Veröffentlichen,
+            Beenden,
+            Beendet
+        }
+    }
+}
