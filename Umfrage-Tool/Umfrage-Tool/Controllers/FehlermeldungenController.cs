eae3cedb4e538c373f1299a5cf413c491483e3db
Toni <Toni5416@aol.com>
2019-05-03 08:11:57 +0200

    Fehlermeldungen
    Status der Umfrage mit dazugehörigen Fehlermeldungen bei nicht Einhaltung

---
+++ Umfrage-Tool/Umfrage-Tool/Controllers/FehlermeldungenController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Umfrage_Tool.Controllers
+{
+    public class FehlermeldungenController : Controller
+    {
+        // GET: Fehlermeldungen
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StatusUmfrageBeendet()
+        {
+            return View();
+        }
+
+        public ActionResult StatusUmfrageLäuft()
+        {
+            return View();
+        }
+
+        public ActionResult StatusUmfrageUnveröffentlicht()
+        {
+            return View();
+        }
+    }
+}
