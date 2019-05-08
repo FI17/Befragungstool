af5b85c361dcd977be997d6ae969a44392870ab4
MaximilianBaer <maximilianteambaer@gmail.com>
2019-05-08 08:02:06 +0200

    Db Änderung Teil 2
    Co-Authored-By: Nils Rücker <nilsrucker@users.noreply.github.com>
    Co-Authored-By: Alexsandr Senaviev <maximilianbaer@users.noreply.github.com>

---
+++ Umfrage-Tool/Domain/Chapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Chapter : Entity
+    {
+        public string text { get; set; }
+        public int position { get; set; }
+        public Survey survey { get; set; }
+
+        public ICollection<Question> questions  { get; set; }
+    }
+    
+}
