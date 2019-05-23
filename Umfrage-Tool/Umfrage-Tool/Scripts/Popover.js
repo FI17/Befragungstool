3c7763f16d8219d7c7e8769e8bd26c6096254f92
DerTut <38212838+DerTut@users.noreply.github.com>
2019-05-23 13:40:16 +0200

    Popover - Home

---
+++ Umfrage-Tool/Umfrage-Tool/Scripts/Popover.js
@@ -0,0 +1,29 @@
+﻿function PopoverAktivieren() {
+    $(document).ready(function () {
+        $("a").popover({
+            trigger: "hover",
+            delay: { show: 700 },
+            placement: 'top'
+        });
+        $("button").popover({
+            trigger: "hover",
+            delay: { show: 700 },
+            placement: 'top'
+        });
+        $("input").not(".input-range").popover({
+            trigger: "hover",
+            delay: { show: 700 },
+            placement: 'top'
+        });
+        $("select").popover({
+            trigger: "hover",
+            delay: { show: 700 },
+            placement: 'right'
+        });
+        $(".input-range").popover({
+            trigger: "hover",
+            delay: { show: 700 },
+            placement: 'left'
+        });
+    });
+}
