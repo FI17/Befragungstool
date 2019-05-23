5c801ff3e70f6a127e9c43c93bdb7d2c05e7f259
CoraxSkeptikos <38212625+CoraxSkeptikos@users.noreply.github.com>
2019-05-23 11:41:21 +0200

    Automatisches Beenden von Umfragen

--- Umfrage-Tool/Umfrage-Tool/Global.asax.cs
+++ Umfrage-Tool/Umfrage-Tool/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Domain.Acces;
+using Domain;
 
 namespace Umfrage_Tool
 {
@@ -12,10 +15,35 @@ namespace Umfrage_Tool
     {
         protected void Application_Start()
         {
+            SetzeTimer();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+        }
+
+        private void SetzeTimer()
+        {
+            var einTimer = new System.Timers.Timer();
+            einTimer.Interval = 108000000;
+            einTimer.Elapsed += SchließeBeendeteUmfragen;
+            einTimer.AutoReset = true;
+            einTimer.Enabled = true;
+        }
+
+        private void SchließeBeendeteUmfragen(object sender, ElapsedEventArgs e)
+        {
+            DatabaseContent db = new DatabaseContent();
+            foreach (var umfrage in db.Surveys)
+            {
+                if (umfrage.states == Survey.States.Öffentlich && umfrage.endTime <= DateTime.Now)
+                {
+                    umfrage.states = Survey.States.Beendet;
+                }
+            }
+
+            db.SaveChanges();
         }
     }
 }
