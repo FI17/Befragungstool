9da12d2a347e6869c7e75512bd3855a8b0a94128
DerTut <38212838+DerTut@users.noreply.github.com>
2019-06-04 10:10:23 +0200

    Update Popover.js

--- Umfrage-Tool/Umfrage-Tool/Scripts/Popover.js
+++ Umfrage-Tool/Umfrage-Tool/Scripts/Popover.js
@@ -1,29 +1,66 @@
 ﻿function PopoverAktivieren() {
     $(document).ready(function () {
-        $("a").popover({
-            trigger: "hover",
+        //    $("a").popover({
+        //        trigger: "hover",
+        //        delay: { show: 700 },
+        //        placement: 'top'
+        //    });
+        //    $("button").popover({
+        //        trigger: "hover",
+        //        delay: { show: 700 },
+        //        placement: 'top'
+        //    });
+        //    $("input").not(".input-range").popover({
+        //        trigger: "hover",
+        //        delay: { show: 700 },
+        //        placement: 'top'
+        //    });
+        //    $("select").popover({
+        //        trigger: "hover",
+        //        delay: { show: 700 },
+        //        placement: 'right'
+        //    });
+        //    $(".input-range").popover({
+        //        trigger: "hover",
+        //        delay: { show: 700 },
+        //        placement: 'left'
+        //});
+
+        var showPopover = function () {
+            $(this).popover("show");
+        };
+        var hidePopover = function () {
+            $(this).popover('hide');
+        };
+
+        $('a').popover({
             delay: { show: 700 },
             placement: 'top'
-        });
-        $("button").popover({
-            trigger: "hover",
+        }).focus(showPopover).focusout(hidePopover).hover(showPopover, hidePopover);
+
+        $('button').popover({
+            trigger: 'manual',
             delay: { show: 700 },
             placement: 'top'
-        });
-        $("input").not(".input-range").popover({
-            trigger: "hover",
+        }).focus(showPopover).focusout(hidePopover).hover(showPopover, hidePopover);
+
+        $('input').not(".input-range").popover({
+            trigger: 'manual',
             delay: { show: 700 },
             placement: 'top'
-        });
-        $("select").popover({
-            trigger: "hover",
+        }).focus(showPopover).focusout(hidePopover).hover(showPopover, hidePopover);
+
+        $('select').popover({
+            trigger: 'manual',
             delay: { show: 700 },
             placement: 'right'
-        });
-        $(".input-range").popover({
-            trigger: "hover",
+        }).focus(showPopover).focusout(hidePopover).hover(showPopover, hidePopover);
+
+        $('.input-range').popover({
+            trigger: 'manual',
             delay: { show: 700 },
             placement: 'left'
-        });
+        }).focus(showPopover).focusout(hidePopover).hover(showPopover, hidePopover);
+
     });
 }
